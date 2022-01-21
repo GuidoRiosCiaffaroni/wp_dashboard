f1c14084730e31f4c7bacdb46b96b89f9baf255f
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-01-21 11:38:57 -0300

    2022_01_21_11_38_55

--- includes/install/install.php
+++ includes/install/install.php
@@ -4,27 +4,28 @@ function pdb_install_flatline()
     
 /* Variables Globales Sistema */
 global $wpdb;                   // Datos del sistema
-
-
-global $tabla_crud;             // Nombre de la tabla de sistema
 global $user_id;                // ID del usuario
 global $status_user;            // Perfil del usuario 
-global $dir_file;               // Nombre de archivo a subir
-global $global_data;            // Almacenamiento de datos Globales
-global $file_name;  
 global $wp_session;             // Inicio sesion variables
 global $global_data;
 
+/* Variables Globales para manejo de archivos */
+global $dir_file;               // Nombre de archivo a subir
+global $global_data;            // Almacenamiento de datos Globales
+
 /* Variables Globales Base de Datos */
+global $tabla_crud;             // Nombre de la tabla de sistema
 global $wpbc_db_version;        // Version del base de datos - utilizado para las actualizaciones
 global $sist_name_flatline;     // Nombre de la tabla de General del sistema 
 
+
+
      
-    $table_name_file = $wpdb->prefix . $sist_name_flatline; 
-    $table_name_departament = $wpdb->prefix . $sist_name_departament;
+    $table_name_flatline = $wpdb->prefix . $sist_name_flatline; 
 
 
-    $sql_file = "CREATE TABLE " . $table_name_file . " (
+
+    $sql_file = "CREATE TABLE " . $table_name_flatline . " (
         id int(11) NOT NULL AUTO_INCREMENT,
         flatline VARCHAR (100) NOT NULL,
         create_at datetime NOT NULL DEFAULT NOW(),
--- index.php
+++ index.php
@@ -22,13 +22,11 @@
 defined( 'ABSPATH' ) or die( '¡Sin trampas!' );
 
 /*Importa funciones de instalacion*/
-
 require_once plugin_dir_path( __FILE__ ) . 'includes/install/install.php';      // Instalacion del Sistema Base de datos
 require_once plugin_dir_path( __FILE__ ) . 'includes/layout/blade.php';         // Funciones para la generacion de Blade 
 
 
 /*Funciones requeridas para administrar y gestionar */
-
 require_once(ABSPATH . "wp-admin" . '/includes/image.php');                     // Funciones para gestionar Imagenes 
 require_once(ABSPATH . "wp-admin" . '/includes/file.php');                      // Funciones para gestionar Archivos
 require_once(ABSPATH . "wp-admin" . '/includes/media.php');                     // Funciones para gestionar Archivos multimadia
@@ -37,25 +35,25 @@ require_once(ABSPATH . 'wp-admin/includes/upgrade.php');                        
 
 /* Variables Globales Sistema */
 global $wpdb;                   // Datos del sistema
-
-
-global $tabla_crud;             // Nombre de la tabla de sistema
 global $user_id;                // ID del usuario
 global $status_user;            // Perfil del usuario 
-global $dir_file;               // Nombre de archivo a subir
-global $global_data;            // Almacenamiento de datos Globales
-global $file_name;  
 global $wp_session;             // Inicio sesion variables
 global $global_data;
 
+/* Variables Globales para manejo de archivos */
+global $dir_file;               // Nombre de archivo a subir
+global $global_data;            // Almacenamiento de datos Globales
+
 /* Variables Globales Base de Datos */
+global $tabla_crud;             // Nombre de la tabla de sistema
 global $wpbc_db_version;        // Version del base de datos - utilizado para las actualizaciones
-global $sist_name_flatline;         // Nombre de la tabla de General del sistema 
+global $sist_name_flatline;     // Nombre de la tabla de General del sistema 
 
 
+
+
+/* Inicio de Variables Globales*/
 $wpbc_db_version = '1.1.0'; 
-
-/*Nombre base de datos*/
 $sist_name_flatline = 'flatline';
 
 
